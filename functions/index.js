258ee2f20f7ed5234caa1105d0f7d1fc1d86a297
satackey <me@satackey.com>
2021-01-19 16:30:22 +0900

    修正

--- functions/index.js
+++ functions/index.js
@@ -123,7 +123,7 @@ exports.join = functions.region('asia-northeast1').https.onRequest(async (req, r
       const alreadyApprovedUserRef = await db.collection(`users`).where(`approvedBy`, `==`, office365Email).get()
       if (!alreadyApprovedUserRef.empty) {
         const user = alreadyApprovedUserRef.docs[0]
-        res.status(400).send(`すでに ${user.data().approvedBy} で認証されたGoogle アカウント ${user.data().invitedTo} が参加済みです`)
+        res.status(400).send(`すでに ${user.data().approvedBy} で認証されたGoogle アカウント ${user.data().sentTo} が参加済みです`)
         return
       }
 
